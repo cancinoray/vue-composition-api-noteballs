b1d557558b4f7ddfa0a80bff29ab69702caa9dfb
Ray Cancino <ray@grandshipper.com>
2022-12-11 22:12:21 +0800

    initial files

---
+++ src/components/Layout/NavBar.vue
@@ -0,0 +1,72 @@
+<template>
+  <nav
+    aria-label="main navigation"
+    class="navbar is-dark"
+    role="navigation"
+  >
+    <div class="container is-max-desktop px-2">
+      <div class="navbar-brand">
+        <div class="navbar-item is-size-5  is-family-monospace">
+          Noteballs
+        </div>
+
+        <a
+          @click.prevent="showMobileNav = !showMobileNav"
+          aria-expanded="false"
+          aria-label="menu"
+          class="navbar-burger"
+          :class="{'is-active' : showMobileNav}"
+          data-target="navbarBasicExample"
+          role="button"
+        >
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+
+    <div
+      class="navbar-menu"
+      :class="{'is-active' : showMobileNav}"
+      id="navbarBasicExample"
+    >
+      <div class="navbar-end">
+        <RouterLink
+          to="/"
+          class="navbar-item"
+          active-class="is-active"
+        >
+          Notes
+        </RouterLink>
+        <RouterLink
+          to="/stats"
+          class="navbar-item"
+          active-class="is-active"          
+        >
+          Stats
+        </RouterLink>
+      </div>
+    </div>
+    </div>
+    
+  </nav>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+//! mobile nav
+const showMobileNav = ref(false)
+
+</script>
+
+<style>
+@media(max-width: 1023px) {
+  .navbar-menu {
+    position: absolute;
+    left: 0;
+    width: 100%;
+
+  }
+}
+</style>
---
+++ src/views/ViewNotes.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="notes">
+    <div
+      class="card mb-4"
+      v-for="i in 3"
+    >
+      <div class="card-content">
+        <div class="content">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloremque architecto quae modi, tenetur excepturi illo tempore dolorum sed atque a eius numquam nisi quidem quo fugit exercitationem sunt quasi molestias!
+        </div>
+      </div>
+      <footer class="card-footer">
+        <a href="#" class="card-footer-item">Edit</a>
+        <a href="#" class="card-footer-item">Delete</a>
+      </footer>
+    </div>
+  </div>
+</template>
